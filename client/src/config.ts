004b5c6c4e8e5dd8ae6f734cd96d5cb85f00e914
philiphabib <philip.habib1@yahoo.com>
2021-10-27 11:40:20 +0200

    modify to meet capstone

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'lnxu11w8lf'
+const apiId = '1tt5jdsy48'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
